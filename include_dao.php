a9dc3bee003558d4afe545882689d2ac4bd86da4
anil-ninja <kumar.anil8892@gmail.com>
2015-05-05 23:48:48 +0530

    test

---
+++ include_dao.php
@@ -0,0 +1,37 @@
+<?php
+	//include all DAO files
+	require_once('class/sql/Connection.class.php');
+	require_once('class/sql/ConnectionFactory.class.php');
+	require_once('class/sql/ConnectionProperty.class.php');
+	require_once('class/sql/QueryExecutor.class.php');
+	require_once('class/sql/Transaction.class.php');
+	require_once('class/sql/SqlQuery.class.php');
+	require_once('class/core/ArrayList.class.php');
+	require_once('class/dao/DAOFactory.class.php');
+ 	
+	require_once('class/dao/CartDAO.class.php');
+	require_once('class/dto/Cart.class.php');
+	require_once('class/mysql/CartMySqlDAO.class.php');
+	require_once('class/mysql/ext/CartMySqlExtDAO.class.php');
+	require_once('class/dao/CategoryDAO.class.php');
+	require_once('class/dto/Category.class.php');
+	require_once('class/mysql/CategoryMySqlDAO.class.php');
+	require_once('class/mysql/ext/CategoryMySqlExtDAO.class.php');
+	require_once('class/dao/OrdersDAO.class.php');
+	require_once('class/dto/Order.class.php');
+	require_once('class/mysql/OrdersMySqlDAO.class.php');
+	require_once('class/mysql/ext/OrdersMySqlExtDAO.class.php');
+	require_once('class/dao/ProductDAO.class.php');
+	require_once('class/dto/Product.class.php');
+	require_once('class/mysql/ProductMySqlDAO.class.php');
+	require_once('class/mysql/ext/ProductMySqlExtDAO.class.php');
+	require_once('class/dao/StoreDAO.class.php');
+	require_once('class/dto/Store.class.php');
+	require_once('class/mysql/StoreMySqlDAO.class.php');
+	require_once('class/mysql/ext/StoreMySqlExtDAO.class.php');
+	require_once('class/dao/UserDetailsDAO.class.php');
+	require_once('class/dto/UserDetail.class.php');
+	require_once('class/mysql/UserDetailsMySqlDAO.class.php');
+	require_once('class/mysql/ext/UserDetailsMySqlExtDAO.class.php');
+
+?>
