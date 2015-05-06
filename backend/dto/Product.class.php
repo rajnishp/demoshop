37e42aa15cb20f1e723274dda85b7d27846601c8
anil-ninja <kumar.anil8892@gmail.com>
2015-05-06 13:33:08 +0530

    remove

--- backend/dto/Product.class.php
+++
@@ -1,20 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'product'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2015-05-05 16:53	 
-	 */
-	class Product{
-		
-		var $id;
-		var $name;
-		var $description;
-		var $sku;
-		var $pricebuy;
-		var $pricesell;
-		var $categoryId;
-		var $imageLink;
-		
-	}
-?>
--- backend/dto/Store.class.php
+++
@@ -1,17 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'store'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2015-05-05 16:53	 
-	 */
-	class Store{
-		
-		var $id;
-		var $name;
-		var $loginId;
-		var $password;
-		var $location;
-		
-	}
-?>
