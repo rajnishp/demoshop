67f097f31f27df26957c626f02ce90236169ad2d
anil-ninja <kumar.anil8892@gmail.com>
2015-05-06 19:17:33 +0530

    modal

--- backend/src/models/Cart.class.php
+++ backend/src/models/Cart.class.php
@@ -2,16 +2,78 @@
 	/**
 	 * Object represents table 'cart'
 	 *
-     	 * @author: http://phpdao.com
+     	 * @author: rajnish
      	 * @date: 2015-05-05 16:53	 
 	 */
 	class Cart{
 		
 		var $id;
-		var $userId;
-		var $orderId;
-		var $productId;
-		var $status;
-		
+		var $userId;
+		var $orderId;
+		var $productId;
+		var $status;
+		
+		function __construct ($userId, $orderId, $productId, $status, $id = null) {
+			$this -> id = $id;
+			$this -> userId = $userId;
+			$this -> orderId = $orderId;
+			$this -> productId = $productId;
+			$this -> status = $status;
+		}
+
+		function setId ($id) {
+			$this -> id = $id;
+		}
+		function getId () {
+			return $this -> id;
+		}
+
+		function setUserId ($userId) {
+			$this -> userId = $userId;
+		}
+		function getUserId () {
+			return $this -> userId;
+		}
+
+		function setOrderId ($orderId) {
+			$this -> orderId = $orderId;
+		}
+		function getOrderId () {
+			return $this -> orderId;
+		}
+
+		function setProductId ($productId) {
+			$this -> productId = $productId;
+		}
+		function getProductId () {
+			return $this -> productId;
+		}
+
+		function setStatus ($status) {
+			$this -> status = $status;
+		}
+		function getStatus () {
+			return $this -> status;
+		}
+
+		function toString () {
+			return $this -> id . ", " . 
+					$this -> userId. ", " . 
+					$this -> orderId. ", " . 
+					$this -> productId. ", " . 
+					$this -> status;
+
+		}
+
+
+		function toArray () {
+			return array (
+						id => $this-> id,
+						userId => $this-> userId,
+						orderId => $this-> orderId,
+						productId => $this-> productId,
+						status => $this-> status
+				);
+		}
 	}
 ?>
--- backend/src/models/Category.class.php
+++ backend/src/models/Category.class.php
@@ -2,15 +2,67 @@
 	/**
 	 * Object represents table 'category'
 	 *
-     	 * @author: http://phpdao.com
+     	 * @author: rajnish
      	 * @date: 2015-05-05 16:53	 
 	 */
 	class Category{
 		
 		var $id;
-		var $name;
-		var $parentId;
-		var $imageLink;
-		
+		var $name;
+		var $parentId;
+		var $imageLink;
+		
+
+		function __construct ($name, $parentId, $imageLink, $id = null) {
+			$this -> id = $id;
+			$this -> name = $name;
+			$this -> parentId = $parentId;
+			$this -> imageLink = $imageLink;
+		}
+
+		function setId ($id) {
+			$this -> id = $id;
+		}
+		function getId () {
+			return $this -> id;
+		}
+
+		function setName ($name) {
+			$this -> name = $name;
+		}
+		function getName () {
+			return $this -> name;
+		}
+
+		function setParentId ($parentId) {
+			$this -> parentId = $parentId;
+		}
+		function getParentId () {
+			return $this -> parentId;
+		}
+
+		function setImageLink ($imageLink) {
+			$this -> imageLink = $imageLink;
+		}
+		function getImageLink () {
+			return $this -> imageLink;
+		}
+
+		function toString () {
+			return $this -> id . ", " . 
+					$this -> name. ", " . 
+					$this -> parentId. ", " . 
+					$this -> imageLink;
+		}
+
+
+		function toArray () {
+			return array (
+						id => $this-> id,
+						name => $this-> name,
+						parentId => $this-> parentId,
+						imageLink => $this-> imageLink
+				);
+		}
 	}
 ?>
