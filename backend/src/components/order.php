2f8d2f12289063fac1d77d6260ba17cdaf1ea26e
rahul lahoria <rahullahoria@CAP-LAP-447.(none)>
2015-05-19 12:13:44 +0530

    view

---
+++ backend/src/components/order.php
@@ -0,0 +1,222 @@
+ <table id="shopping-cart-table" class="cart-table data-table">
+            <col width="1" />
+            <col width="1" />
+            <col width="1" />
+            <col width="1" />
+            <col width="1" />
+            <col width="1" />
+            
+                    <thead>
+                <tr>
+                    <th rowspan="1"><span class="nobr">Product</span></th>
+
+                    <th rowspan="1">&nbsp;</th>
+
+                    <th class="a-center cart-price-head" colspan="1">
+<!--                            <div class="cart-price-placeholder">-->
+                            <span class="nobr">Price</span>
+<!--                            </div>-->
+                    </th>
+
+                    <th rowspan="1" class="a-center">
+                        Qty
+                        
+                    </th>
+
+                    <th class="a-center cart-total-head" colspan="1">
+<!--                            <div class="cart-total-placeholder">-->
+                            Subtotal<!--                            </div>-->
+                    </th>
+
+                    <th class="a-center" rowspan="1">&nbsp;</th>
+
+                </tr>
+                            </thead>
+            <tfoot>
+                <tr>
+                    <td colspan="50" class="a-right cart-footer-actions">
+
+                        <!-- Make this button the first responder rather than empty cart -->
+                        <button type="submit" style="visibility:hidden;" name="update_cart_action" value="update_qty" title="Update Shopping Cart" class="button2 btn-update"><span><span>Update Shopping Cart</span></span></button>
+
+                        <button type="submit" name="update_cart_action" value="empty_cart" title="Empty Cart" class="button2 btn-empty" id="empty_cart_button"><span><span>Empty Cart</span></span></button>
+
+                        <button type="submit" name="update_cart_action" value="update_qty" title="Update Shopping Cart" class="button2 btn-update"><span><span>Update Shopping Cart</span></span></button>
+
+                        <span class="or">-or-</span>
+
+                                                    <button type="button" title="Continue Shopping" class="button2 btn-continue" onclick="setLocation('http://loc.dpower4.com/magento/men/tees-knits-and-polos/chelsea-tee-642.html')"><span><span>Continue Shopping</span></span></button>
+                                                <!--[if lt IE 8]>
+                        <input type="hidden" id="update_cart_action_container" />
+                        <script type="text/javascript">
+                        //<![CDATA[
+                            Event.observe(window, 'load', function()
+                            {
+                                // Internet Explorer (lt 8) does not support value attribute in button elements
+                                $emptyCartButton = $('empty_cart_button');
+                                $cartActionContainer = $('update_cart_action_container');
+                                if ($emptyCartButton && $cartActionContainer) {
+                                    Event.observe($emptyCartButton, 'click', function()
+                                    {
+                                        $emptyCartButton.setAttribute('name', 'update_cart_action_temp');
+                                        $cartActionContainer.setAttribute('name', 'update_cart_action');
+                                        $cartActionContainer.setValue('empty_cart');
+                                    });
+                                }
+
+                            });
+                        //]]>
+                        </script>
+                        <![endif]-->
+                    </td>
+                </tr>
+            </tfoot>
+            <tbody>
+                            <tr>
+    <td class="product-cart-image">
+                    <a href="http://loc.dpower4.com/magento/tori-tank-639.html" title="Tori Tank" class="product-image">
+                    <img src="http://loc.dpower4.com/magento/media/catalog/product/cache/1/thumbnail/180x/9df78eab33525d08d6e5fb8d27136e95/w/b/wbk003t.jpg" alt="Tori Tank" />
+                    </a>
+        
+        <ul class="cart-links">
+                            <li>
+                    <a href="http://loc.dpower4.com/magento/checkout/cart/configure/id/2480/" title="Edit item parameters">Edit</a>
+                </li>
+            
+                    </ul>
+    </td>
+
+    <td class="product-cart-info">
+
+        <a href="http://loc.dpower4.com/magento/checkout/cart/delete/id/2480/uenc/aHR0cDovL2xvYy5kcG93ZXI0LmNvbS9tYWdlbnRvL2NoZWNrb3V0L2NhcnQv/" title="Remove Item" class="btn-remove btn-remove2">Remove Item</a>
+
+        <h2 class="product-name">
+                    <a href="http://loc.dpower4.com/magento/tori-tank-639.html">Tori Tank</a>
+                </h2>
+
+        <div class="product-cart-sku">
+            <span class="label">SKU:</span> wbk005        </div>
+
+                <dl class="item-options">
+                                        <dt>Color</dt>
+                <dd>Indigo                            </dd>
+                                        <dt>Size</dt>
+                <dd>L                            </dd>
+                    </dl>
+        
+        
+                                    
+    </td>
+
+    
+                <td class="product-cart-price" data-rwd-label="Price" data-rwd-tax-label="Excl. Tax">
+
+                            <span class="cart-price">
+                                                <span class="price">$60.00</span>                
+            </span>
+
+                    </td>
+        <!-- inclusive price starts here -->
+                <td class="product-cart-actions" data-rwd-label="Qty">
+        <input type="text" pattern="\d*" name="cart[2480][qty]" value="1" size="4" title="Qty" class="input-text qty" maxlength="12" />
+
+        <button type="submit" name="update_cart_action" value="update_qty" title="Update" class="button btn-update"><span><span>Update</span></span>
+        </button>
+
+        <ul class="cart-links">
+                            <li>
+                    <a href="http://loc.dpower4.com/magento/checkout/cart/configure/id/2480/" title="Edit item parameters">Edit</a>
+                </li>
+            
+                    </ul>
+
+    </td>
+
+    <!--Sub total starts here -->
+        <td class="product-cart-total" data-rwd-label="Subtotal">
+                    <span class="cart-price">
+        
+                                                <span class="price">$60.00</span>                            
+        </span>
+            </td>
+            <td class="a-center product-cart-remove">
+        <a href="http://loc.dpower4.com/magento/checkout/cart/delete/id/2480/uenc/aHR0cDovL2xvYy5kcG93ZXI0LmNvbS9tYWdlbnRvL2NoZWNrb3V0L2NhcnQv/" title="Remove Item" class="btn-remove btn-remove2">Remove Item</a>
+    </td>
+</tr>
+
+
+                            <tr>
+    <td class="product-cart-image">
+                    <a href="http://loc.dpower4.com/magento/chelsea-tee-782.html" title="Chelsea Tee" class="product-image">
+                    <img src="http://loc.dpower4.com/magento/media/catalog/product/cache/1/thumbnail/180x/9df78eab33525d08d6e5fb8d27136e95/m/t/mtk002t.jpg" alt="Chelsea Tee" />
+                    </a>
+        
+        <ul class="cart-links">
+                            <li>
+                    <a href="http://loc.dpower4.com/magento/checkout/cart/configure/id/2482/" title="Edit item parameters">Edit</a>
+                </li>
+            
+                    </ul>
+    </td>
+
+    <td class="product-cart-info">
+
+        <a href="http://loc.dpower4.com/magento/checkout/cart/delete/id/2482/uenc/aHR0cDovL2xvYy5kcG93ZXI0LmNvbS9tYWdlbnRvL2NoZWNrb3V0L2NhcnQv/" title="Remove Item" class="btn-remove btn-remove2">Remove Item</a>
+
+        <h2 class="product-name">
+                    <a href="http://loc.dpower4.com/magento/chelsea-tee-782.html">Chelsea Tee</a>
+                </h2>
+
+        <div class="product-cart-sku">
+            <span class="label">SKU:</span> mtk000l        </div>
+
+                <dl class="item-options">
+                                        <dt>Color</dt>
+                <dd>Blue                            </dd>
+                                        <dt>Size</dt>
+                <dd>L                            </dd>
+                    </dl>
+        
+        
+                                    
+    </td>
+
+    
+                <td class="product-cart-price" data-rwd-label="Price" data-rwd-tax-label="Excl. Tax">
+
+                            <span class="cart-price">
+                                                <span class="price">$75.00</span>                
+            </span>
+
+                    </td>
+        <!-- inclusive price starts here -->
+                <td class="product-cart-actions" data-rwd-label="Qty">
+        <input type="text" pattern="\d*" name="cart[2482][qty]" value="2" size="4" title="Qty" class="input-text qty" maxlength="12" />
+
+        <button type="submit" name="update_cart_action" value="update_qty" title="Update" class="button btn-update"><span><span>Update</span></span>
+        </button>
+
+        <ul class="cart-links">
+                            <li>
+                    <a href="http://loc.dpower4.com/magento/checkout/cart/configure/id/2482/" title="Edit item parameters">Edit</a>
+                </li>
+            
+                    </ul>
+
+    </td>
+
+    <!--Sub total starts here -->
+        <td class="product-cart-total" data-rwd-label="Subtotal">
+                    <span class="cart-price">
+        
+                                                <span class="price">$150.00</span>                            
+        </span>
+            </td>
+            <td class="a-center product-cart-remove">
+        <a href="http://loc.dpower4.com/magento/checkout/cart/delete/id/2482/uenc/aHR0cDovL2xvYy5kcG93ZXI0LmNvbS9tYWdlbnRvL2NoZWNrb3V0L2NhcnQv/" title="Remove Item" class="btn-remove btn-remove2">Remove Item</a>
+    </td>
+</tr>
+
+
+                        </tbody>
+        </table>
